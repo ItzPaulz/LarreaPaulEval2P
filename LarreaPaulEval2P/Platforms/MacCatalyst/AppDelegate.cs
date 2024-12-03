4b7eaadd99fc37c086976bde03139340023ef24e
laboratorio <laboratorio@UPOAULA10315>
2024-12-03 07:20:03 -0500

    Agregar archivos de proyecto.

---
+++ LarreaPaulEval2P/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LarreaPaulEval2P
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
