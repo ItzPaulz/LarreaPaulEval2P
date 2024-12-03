ab71264bac76b9d879acd363d5ff92b93022d9e8
ItzPaulz <paullarrea2004@gmail.com>
2024-12-03 08:27:56 -0500

    He creado las carpetas de interface y modelo, por 5nta vez, PROFESOR SE ME REINICIO LA MAQUINA Y CAMBIE DE MAQUINA TENGA PIEDAD POR FAVOR

---
+++ LarreaPaulEval2P/Interfaz/ILarrea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LarreaPaulEval2P.Modelo;
+
+namespace LarreaPaulEval2P.Interfaz
+{
+    public interface ILarrea
+    {
+        Larrea GetRecarga(string numero);
+        bool CreateRecarga(Larrea recarga);
+        bool UpdateRecarga(Larrea recarga);
+        bool DeleteRecarga(string numero);
+    }
+}
+}
---
+++ LarreaPaulEval2P/Modelo/Larrea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LarreaPaulEval2P.Modelo
+{
+    public class Larrea
+    {
+        public string Telefono { get; set; }
+        public string Nombre { get; set; }
+    }
+}
